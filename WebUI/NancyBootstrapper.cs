931aa2b31a9c2cc68d2b654b9bd0e40eda3dd249
Peter Shaw <shawty_ds@yahoo.com>
2015-10-23 19:32:23 +0100

    Initial Project Creation

---
+++ WebUI/NancyBootstrapper.cs
@@ -0,0 +1,19 @@
+﻿using Nancy;
+using Nancy.Conventions;
+
+namespace WebUI
+{
+  public class NancyBootstrapper : DefaultNancyBootstrapper
+  {
+    protected override void ConfigureConventions(NancyConventions nancyConventions)
+    {
+      base.ConfigureConventions(nancyConventions);
+
+      Conventions.StaticContentsConventions.Add(StaticContentConventionBuilder.AddDirectory("/scripts", @"scripts"));
+      Conventions.StaticContentsConventions.Add(StaticContentConventionBuilder.AddDirectory("/styles", @"styles"));
+      Conventions.StaticContentsConventions.Add(StaticContentConventionBuilder.AddDirectory("/images", @"images"));
+      Conventions.StaticContentsConventions.Add(StaticContentConventionBuilder.AddDirectory("/fonts", @"fonts"));
+
+    }
+  }
+}
---
+++ WebUI/RouteModules/RootRoutes.cs
@@ -0,0 +1,18 @@
+﻿using Nancy;
+
+namespace WebUI.RouteModules
+{
+  public class RootRoutes : NancyModule
+  {
+    public RootRoutes()
+    {
+      Get["/"] = Index;
+    }
+
+    private dynamic Index(dynamic parameters)
+    {
+      return View["root/index"];
+    }
+
+  }
+}
