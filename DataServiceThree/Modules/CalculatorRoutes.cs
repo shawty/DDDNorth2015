3131a585f9f3888b733fb60a47c670a67de19489
Peter Shaw <shawty_ds@yahoo.com>
2015-10-24 11:28:57 +0100

    Routes and Logic added to service 3

---
+++ DataServiceThree/Modules/CalculatorRoutes.cs
@@ -0,0 +1,48 @@
+﻿using Nancy;
+
+namespace DataServiceThree.Modules
+{
+  public class CalculatorRoutes : NancyModule
+  {
+    public CalculatorRoutes() : base("calculator")
+    {
+      Get["/addition/{src}/{operand}"] = Addition;
+      Get["/subtraction/{src}/{operand}"] = Subtraction;
+      Get["/multipication/{src}/{operand}"] = Multipication;
+      Get["/division/{src}/{operand}"] = Division;
+    }
+
+    private dynamic Addition(dynamic parameters)
+    {
+      decimal source = parameters.src;
+      decimal operand = parameters.operand;
+
+      return Response.AsJson(new {Result = source + operand});
+    }
+
+    private dynamic Subtraction(dynamic parameters)
+    {
+      decimal source = parameters.src;
+      decimal operand = parameters.operand;
+
+      return Response.AsJson(new { Result = source - operand });
+    }
+
+    private dynamic Multipication(dynamic parameters)
+    {
+      decimal source = parameters.src;
+      decimal operand = parameters.operand;
+
+      return Response.AsJson(new { Result = source * operand });
+    }
+
+    private dynamic Division(dynamic parameters)
+    {
+      decimal source = parameters.src;
+      decimal operand = parameters.operand;
+
+      return Response.AsJson(new { Result = source / operand });
+    }
+
+  }
+}
