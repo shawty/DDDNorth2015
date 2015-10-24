187caf2392ab040a6d3db73356198173dc43aca6
Peter Shaw <shawty_ds@yahoo.com>
2015-10-24 11:12:51 +0100

    Added logic to service two

---
+++ DataServiceTwo/Modules/RootRoutes.cs
@@ -0,0 +1,30 @@
+﻿using Nancy;
+
+namespace DataServiceTwo.Modules
+{
+  public class RootRoutes : NancyModule
+  {
+    public RootRoutes()
+    {
+      Get["/"] = Index;
+      Get["/machineinfo"] = MachineInfo;
+      Get["/userinfo"] = UserInfo;
+    }
+
+    private dynamic Index(dynamic parameters)
+    {
+      return Response.AsJson(new {ServiceName = "Service Two"});
+    }
+
+    private dynamic MachineInfo(dynamic parameters)
+    {
+      return Response.AsJson(new { MachineName = "lucinda-mobile", OperatingSystem = "Windows 7 (64bit)" });
+    }
+
+    private dynamic UserInfo(dynamic parameters)
+    {
+      return Response.AsJson(new { UserName = "shawty", Workgroup = "digisolutions" });
+    }
+
+  }
+}
